835ce80f68a9d514ea5b922531dce856bbb0a518
jfcarocota <jfcarocota@gmail.com>
2019-03-08 14:41:18 -0700

    follow move fwd y animacion

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -18,6 +18,9 @@ public class NPC : MonoBehaviour
     [SerializeField]
     Transform follow;
 
+    [SerializeField, Range(0f, 10f)]
+    float maxDistanceFollow = 2f;
+
     protected void Update()
     {
         if (isPartner)
@@ -39,12 +42,25 @@ public class NPC : MonoBehaviour
 
     void PartnerBeahaviour()
     {
-        transform.LookAt(follow);
+        bool followLeader = Vector3.Distance(transform.position, 
+            follow.position) >= maxDistanceFollow;
+
+        if (followLeader)
+        {
+            transform.LookAt(follow);
+            transform.Translate(Vector3.forward *
+                moveSpeed * Time.deltaTime);
+            anim.SetFloat("move", 1);
+        }
+        else
+        {
+            anim.SetFloat("move", 0);
+        }
     }
 
     void NPCBeahaviour()
     {
-
+        
     }
 
     void LeaderBeahaviour()
