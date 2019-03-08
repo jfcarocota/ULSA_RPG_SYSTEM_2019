defc03601f001780b87bf8bf4c8933b21c159593
jfcarocota <jfcarocota@gmail.com>
2019-03-08 13:08:10 -0700

    commit 08032019
    Player controller basics

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Movement.Sys3D;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)]
+    float moveSpeed = 0f;
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Movement3D.MoveFacing(transform, moveSpeed);
+        anim.SetFloat("move", Mathf.Abs(Movement3D.Axis.magnitude));
+        Debug.Log(Mathf.Abs(Movement3D.Axis.magnitude));
+    }
+}
