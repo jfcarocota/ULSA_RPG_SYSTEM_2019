d8c6401b16d157f1c05fd1fe2b3684196658f1bd
jfcarocota <jfcarocota@gmail.com>
2019-03-08 14:17:14 -0700

    look at y npc

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Movement.Sys3D;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField]
+    protected bool isPartner;
+    [SerializeField]
+    protected bool isLeader;
+
+    [SerializeField, Range(0f, 10f)]
+    protected float moveSpeed = 0f;
+
+    protected Animator anim;
+
+    [SerializeField]
+    Transform follow;
+
+    protected void Update()
+    {
+        if (isPartner)
+        {
+            if (isLeader)
+            {
+                LeaderBeahaviour();
+            }
+            else
+            {
+                PartnerBeahaviour();
+            }
+        }
+        else
+        {
+            NPCBeahaviour();
+        }
+    }
+
+    void PartnerBeahaviour()
+    {
+        transform.LookAt(follow);
+    }
+
+    void NPCBeahaviour()
+    {
+
+    }
+
+    void LeaderBeahaviour()
+    {
+        Movement3D.MoveFacing(transform, moveSpeed);
+        anim.SetFloat("move", Mathf.Abs(Movement3D.Axis.magnitude));
+        Debug.Log(Mathf.Abs(Movement3D.Axis.magnitude));
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,22 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Core.Movement.Sys3D;
 
-public class Player : MonoBehaviour
+public class Player : NPC
 {
-    [SerializeField, Range(0f, 10f)]
-    float moveSpeed = 0f;
-
-    Animator anim;
+   
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
     }
 
-    void Update()
+    new void Update()
     {
-        Movement3D.MoveFacing(transform, moveSpeed);
-        anim.SetFloat("move", Mathf.Abs(Movement3D.Axis.magnitude));
-        Debug.Log(Mathf.Abs(Movement3D.Axis.magnitude));
+        base.Update();
     }
 }
